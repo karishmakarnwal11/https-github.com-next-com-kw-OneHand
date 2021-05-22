d1db6d566c68f09d46788e6ef352dd01e05e41ac
Karishma <karishma@prminfotech.com>
2021-05-22 11:54:00 +0530

    Add project files.

---
+++ OnHandWeb/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnHandWeb.Models;
+
+namespace OnHandWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View("About");
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult BetweenNeedyAndDonor()
+        {
+            return View();
+        }
+        public IActionResult FromTheirHandsToYours()
+        {
+            return View();
+        }
+        public IActionResult Idea()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult TermsAndConditions()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
